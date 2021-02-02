7ab3067555b5271fea269fcd65b8a9f99aa3f84c
Maciej Howański <howanski@windowsmail.com>
2021-02-02 17:28:45 +0100

    fetch data in background, show lightning fast

--- bg-worker.js
+++ bg-worker.js
@@ -1,1 +1,212 @@
-// All I do here is help to debug, I love my job!
+(function () {
+  let localData = {};
+
+  function storeDataInLocalStorage(continueFunction) {
+    let promise = browser.storage.local.set({ data: localData });
+    if (continueFunction) {
+      promise.then(continueFunction);
+    }
+  }
+
+  function isDataFetchedFromModem() {
+    return (
+      (localData.currentMonthMovementInBytes > 0) &
+      (localData.monthlyDataCapInBytes > 0)
+    );
+  }
+
+  function milisecondsToFullDays(miliseconds) {
+    return parseInt(miliseconds / (1000 * 60 * 60 * 24));
+  }
+
+  function getCurrentMonthBorders() {
+    // date magic, don't read code while sober
+    let nowExactly = new Date();
+
+    let currentDayOfMonth = nowExactly.getDate();
+    let currentMonth = nowExactly.getMonth(); // [0-11]
+    let currentYear = nowExactly.getFullYear();
+
+    let timeOfStart = 0; // for destructor
+    let timeOfEnd = 0;
+
+    if (localData.startOfMonthDay <= currentDayOfMonth) {
+      // between invoice day and last day of physical month
+      timeOfStart = new Date(
+        currentYear,
+        currentMonth,
+        localData.startOfMonthDay
+      );
+      if (currentMonth != 11) {
+        timeOfEnd = new Date(
+          currentYear,
+          currentMonth + 1,
+          localData.startOfMonthDay
+        );
+      } else {
+        timeOfEnd = new Date(currentYear + 1, 0, localData.startOfMonthDay);
+      }
+    } else {
+      if (currentMonth != 0) {
+        timeOfStart = new Date(
+          currentYear,
+          currentMonth - 1,
+          localData.startOfMonthDay
+        );
+      } else {
+        timeOfStart = new Date(currentYear - 1, 11, localData.startOfMonthDay);
+      }
+      timeOfEnd = new Date(
+        currentYear,
+        currentMonth,
+        localData.startOfMonthDay
+      );
+    }
+    return [timeOfStart, timeOfEnd, nowExactly];
+  }
+
+  function recalculateStatistics() {
+    if (isDataFetchedFromModem()) {
+      localData.bytesLeftThisMonth =
+        localData.monthlyDataCapInBytes - localData.currentMonthMovementInBytes;
+
+      let [timeOfStart, timeOfEnd, nowExactly] = getCurrentMonthBorders();
+
+      let thisMonthLengthInMiliseconds = timeOfEnd - timeOfStart;
+      let thisMonthLengthInDays = milisecondsToFullDays(
+        thisMonthLengthInMiliseconds
+      );
+      let dailyConsumableBytes =
+        localData.monthlyDataCapInBytes / thisMonthLengthInDays;
+
+      let virtualBytesToConsume = localData.monthlyDataCapInBytes;
+
+      localData.milisecondsLeftThisMonth =
+        timeOfEnd.getTime() - nowExactly.getTime();
+
+      localData.steadyTransferToEndOfMonthInKbps = parseInt(
+        localData.bytesLeftThisMonth /
+          1024 /
+          (localData.milisecondsLeftThisMonth / 1000)
+      );
+
+      let crawledToToday = false;
+      let tosEpoh = timeOfStart.getTime();
+      let healthySchedule = [];
+      for (i = 0; i < thisMonthLengthInDays; i++) {
+        //iterating through days of current month
+        virtualBytesToConsume -= dailyConsumableBytes;
+        let virtualDate = new Date(tosEpoh + i * 24 * 60 * 60 * 1000);
+        if (
+          virtualDate.getDate() == nowExactly.getDate() &&
+          virtualDate.getMonth() == nowExactly.getMonth()
+        ) {
+          crawledToToday = true;
+          localData.bytesLeftToday =
+            localData.bytesLeftThisMonth - virtualBytesToConsume;
+        }
+
+        if (crawledToToday) {
+          healthySchedule[healthySchedule.length] = {
+            date: virtualDate.getTime(),
+            transferLeftOnEod: parseInt(virtualBytesToConsume),
+          };
+        }
+      }
+      localData.healthySchedule = healthySchedule;
+      storeDataInLocalStorage();
+    }
+  }
+
+  function fetchCurrentMonthData() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let responseData = this.responseXML.firstChild;
+        let responseDataVars = responseData.children;
+        localData.currentMonthMovementInBytes = 0;
+        for (i = 0; i < responseDataVars.length; i++) {
+          let currNode = responseDataVars[i];
+          let tagName = currNode.tagName;
+          let nodeValue = currNode.innerHTML;
+          if (tagName == "CurrentMonthDownload") {
+            localData.currentMonthMovementInBytes += parseInt(nodeValue);
+          } else if (tagName == "CurrentMonthUpload") {
+            localData.currentMonthMovementInBytes += parseInt(nodeValue);
+          }
+        }
+        storeDataInLocalStorage(recalculateStatistics);
+      }
+    };
+    httpRequest.open(
+      "GET",
+      "http://192.168.1.1/api/monitoring/month_statistics",
+      true
+    );
+    httpRequest.overrideMimeType("text/xml");
+    httpRequest.send();
+  }
+
+  function fetchOverallData() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        responseData = this.responseXML.firstChild;
+        responseDataVars = responseData.children;
+        for (i = 0; i < responseDataVars.length; i++) {
+          let currNode = responseDataVars[i];
+          let tagName = currNode.tagName;
+          let nodeValue = currNode.innerHTML;
+          if (tagName == "trafficmaxlimit") {
+            localData.monthlyDataCapInBytes = parseInt(nodeValue);
+          } else if (tagName == "StartDay") {
+            localData.startOfMonthDay = parseInt(nodeValue);
+          }
+        }
+        storeDataInLocalStorage();
+      }
+    };
+    httpRequest.open(
+      "GET",
+      "http://192.168.1.1/api/monitoring/start_date",
+      true
+    );
+    httpRequest.overrideMimeType("text/xml");
+    httpRequest.send();
+  }
+
+  function makeFullDataProcess() {
+    function processKnownData(dataSavedInLocalStorage) {
+      if (localData) {
+      } else {
+        localData = dataSavedInLocalStorage.data;
+      }
+
+      if (localData) {
+      } else {
+        localData = {
+          currentMonthMovementInBytes: 0,
+          monthlyDataCapInBytes: 0,
+          startOfMonthDay: 1,
+          bytesLeftThisMonth: 0,
+          bytesLeftToday: 0,
+          dailyConsumableBytes: 0,
+          milisecondsLeftThisMonth: 0,
+          steadyTransferToEndOfMonthInKbps: 0,
+          healthySchedule: [],
+        };
+      }
+      fetchCurrentMonthData();
+      fetchOverallData();
+    }
+    browser.storage.local.get("data").then(processKnownData);
+    storeDataInLocalStorage();
+  }
+
+  function appRunner() {
+    browser.storage.local.clear();
+    makeFullDataProcess();
+    setInterval(makeFullDataProcess, 10000);
+  }
+  appRunner();
+})();
--- html/main.js
+++ html/main.js
@@ -15,240 +15,66 @@
     return (bytes / (1024 * 1024 * 1024)).toFixed(2);
   }
 
-  function milisecondsToFullDays(miliseconds) {
-    return parseInt(miliseconds / (1000 * 60 * 60 * 24));
-  }
-
-  function isDataFetchedFromModem() {
-    return (
-      (mainInfo.currentMonthMovementInBytes > 0) &
-      (mainInfo.monthlyDataCapInBytes > 0)
-    );
-  }
-
-  function recalculateStatistics() {
-    if (isDataFetchedFromModem()) {
-      let jackpot = mainInfo.monthlyDataCapInBytes;
-      let hand = mainInfo.currentMonthMovementInBytes;
-      mainInfo.bytesLeftThisMonth =
-        mainInfo.monthlyDataCapInBytes - mainInfo.currentMonthMovementInBytes;
-
-      let [timeOfStart, timeOfEnd, nowExactly] = getCurrentMonthBorders();
-
-      let thisMonthLengthInMiliseconds = timeOfEnd - timeOfStart;
-      let thisMonthLengthInDays = milisecondsToFullDays(
-        thisMonthLengthInMiliseconds
-      );
-      let dailyConsumableBytes =
-        mainInfo.monthlyDataCapInBytes / thisMonthLengthInDays;
-
-      let virtualBytesToConsume = mainInfo.monthlyDataCapInBytes;
-
-      mainInfo.milisecondsLeftThisMonth =
-        timeOfEnd.getTime() - nowExactly.getTime();
-
-      mainInfo.steadyTransferToEndOfMonthInKbps = parseInt(
-        mainInfo.bytesLeftThisMonth /
-          1024 /
-          (mainInfo.milisecondsLeftThisMonth / 1000)
-      );
-
-      let crawledToToday = false;
-      let tosEpoh = timeOfStart.getTime();
-      let healthySchedule = [];
-      for (i = 0; i < thisMonthLengthInDays; i++) {
-        //iterating through days of current month
-        virtualBytesToConsume -= dailyConsumableBytes;
-        let virtualDate = new Date(tosEpoh + i * 24 * 60 * 60 * 1000);
-        if (
-          virtualDate.getDate() == nowExactly.getDate() &&
-          virtualDate.getMonth() == nowExactly.getMonth()
-        ) {
-          crawledToToday = true;
-          mainInfo.bytesLeftToday =
-            mainInfo.bytesLeftThisMonth - virtualBytesToConsume;
-        }
-
-        if (crawledToToday) {
-          healthySchedule[healthySchedule.length] = {
-            date: virtualDate,
-            transferLeftOnEod: virtualBytesToConsume,
-          };
-        }
-      }
-      mainInfo.healthySchedule = healthySchedule;
-    }
-  }
-
-  function getCurrentMonthBorders() {
-    // date magic, don't read code while sober
-    let nowExactly = new Date();
-
-    let currentDayOfMonth = nowExactly.getDate();
-    let currentMonth = nowExactly.getMonth(); // [0-11]
-    let currentYear = nowExactly.getFullYear();
-
-    let timeOfStart = 0; // for destructor
-    let timeOfEnd = 0;
-
-    if (mainInfo.startOfMonthDay <= currentDayOfMonth) {
-      // between invoice day and last day of physical month
-      timeOfStart = new Date(
-        currentYear,
-        currentMonth,
-        mainInfo.startOfMonthDay
-      );
-      if (currentMonth != 11) {
-        timeOfEnd = new Date(
-          currentYear,
-          currentMonth + 1,
-          mainInfo.startOfMonthDay
-        );
-      } else {
-        timeOfEnd = new Date(currentYear + 1, 0, mainInfo.startOfMonthDay);
-      }
-    } else {
-      if (currentMonth != 0) {
-        timeOfStart = new Date(
-          currentYear,
-          currentMonth - 1,
-          mainInfo.startOfMonthDay
-        );
-      } else {
-        timeOfStart = new Date(currentYear - 1, 11, mainInfo.startOfMonthDay);
-      }
-      timeOfEnd = new Date(currentYear, currentMonth, mainInfo.startOfMonthDay);
-    }
-    return [timeOfStart, timeOfEnd, nowExactly];
-  }
-
   function updatepopUpInterface() {
-    if (isDataFetchedFromModem()) {
-      recalculateStatistics();
+    let readableMonthLeftElem = document.getElementById("month-data-left");
+    readableMonthLeftElem.innerHTML =
+      bytesToGigaBytes(mainInfo.bytesLeftThisMonth) + " GB";
 
-      let readableMonthLeftElem = document.getElementById("month-data-left");
-      readableMonthLeftElem.innerHTML =
-        bytesToGigaBytes(mainInfo.bytesLeftThisMonth) + " GB";
+    let readableMonthLeftProgressElem = document.getElementById(
+      "month-data-left-progress"
+    );
+    readableMonthLeftProgressElem.setAttribute(
+      "max",
+      mainInfo.monthlyDataCapInBytes
+    );
+    readableMonthLeftProgressElem.setAttribute(
+      "value",
+      mainInfo.bytesLeftThisMonth
+    );
 
-      let readableMonthLeftProgressElem = document.getElementById(
-        "month-data-left-progress"
+    let steadyTransferAdvise = document.getElementById(
+      "steady-transfer-till-eom"
+    );
+    steadyTransferAdvise.innerHTML =
+      parseInt(mainInfo.steadyTransferToEndOfMonthInKbps) + " kB/s";
+
+    let readableTodayLeftElem = document.getElementById("today-data-left");
+    readableTodayLeftElem.innerHTML =
+      bytesToGigaBytes(mainInfo.bytesLeftToday) + " GB";
+
+    let tableElem = document.getElementById("main-table");
+    for (i = 0; i < mainInfo.healthySchedule.length; i++) {
+      // Print schedule for rest of the month
+      let newRow = tableElem.insertRow(-1);
+
+      let newCell = newRow.insertCell(0);
+      let dateObject = new Date(mainInfo.healthySchedule[i].date);
+      let newText = document.createTextNode(
+        dateObject.getDate() + "." + (dateObject.getMonth() + 1)
       );
-      readableMonthLeftProgressElem.setAttribute(
-        "max",
-        mainInfo.monthlyDataCapInBytes
+      newCell.appendChild(newText);
+
+      let newCell2 = newRow.insertCell(1);
+      let newText2 = document.createTextNode(
+        bytesToGigaBytes(mainInfo.healthySchedule[i].transferLeftOnEod) + " GB"
       );
-      readableMonthLeftProgressElem.setAttribute(
+      newCell2.appendChild(newText2);
+
+      let progressCell = newRow.insertCell(2);
+      let progressElem = document.createElement("progress");
+      progressElem.setAttribute("max", mainInfo.monthlyDataCapInBytes);
+      progressElem.setAttribute(
         "value",
-        mainInfo.bytesLeftThisMonth
+        mainInfo.healthySchedule[i].transferLeftOnEod
       );
-
-      let steadyTransferAdvise = document.getElementById(
-        "steady-transfer-till-eom"
-      );
-      steadyTransferAdvise.innerHTML =
-        parseInt(mainInfo.steadyTransferToEndOfMonthInKbps) + " kB/s";
-
-      let readableTodayLeftElem = document.getElementById("today-data-left");
-      readableTodayLeftElem.innerHTML =
-        bytesToGigaBytes(mainInfo.bytesLeftToday) + " GB";
-
-      let tableElem = document.getElementById("main-table");
-      for (i = 0; i < mainInfo.healthySchedule.length; i++) {
-        // Print schedule for rest of the month
-        let newRow = tableElem.insertRow(-1);
-
-        let newCell = newRow.insertCell(0);
-        let newText = document.createTextNode(
-          mainInfo.healthySchedule[i].date.getDate() +
-            "." +
-            (mainInfo.healthySchedule[i].date.getMonth() + 1)
-        );
-        newCell.appendChild(newText);
-
-        let newCell2 = newRow.insertCell(1);
-        let newText2 = document.createTextNode(
-          bytesToGigaBytes(mainInfo.healthySchedule[i].transferLeftOnEod) +
-            " GB"
-        );
-        newCell2.appendChild(newText2);
-
-        let progressCell = newRow.insertCell(2);
-        let progressElem = document.createElement("progress");
-        progressElem.setAttribute("max", mainInfo.monthlyDataCapInBytes);
-        progressElem.setAttribute(
-          "value",
-          mainInfo.healthySchedule[i].transferLeftOnEod
-        );
-        progressCell.appendChild(progressElem);
-      }
+      progressCell.appendChild(progressElem);
     }
   }
 
-  function fetchCurrentMonthData() {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        let responseData = this.responseXML.firstChild;
-        let responseDataVars = responseData.children;
-        mainInfo.currentMonthMovementInBytes = 0;
-        for (i = 0; i < responseDataVars.length; i++) {
-          let currNode = responseDataVars[i];
-          let tagName = currNode.tagName;
-          let nodeValue = currNode.innerHTML;
-          if (tagName == "CurrentMonthDownload") {
-            mainInfo.currentMonthMovementInBytes += parseInt(nodeValue);
-          } else if (tagName == "CurrentMonthUpload") {
-            mainInfo.currentMonthMovementInBytes += parseInt(nodeValue);
-          }
-        }
-        updatepopUpInterface();
-      }
-    };
-    httpRequest.open(
-      "GET",
-      "http://192.168.1.1/api/monitoring/month_statistics",
-      true
-    );
-    httpRequest.overrideMimeType("text/xml");
-    httpRequest.send();
+  function loadDataAndShow(dataSavedInLocalStorage) {
+    mainInfo = dataSavedInLocalStorage.data;
+    updatepopUpInterface();
   }
 
-  function fetchOverallData() {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        responseData = this.responseXML.firstChild;
-        responseDataVars = responseData.children;
-        for (i = 0; i < responseDataVars.length; i++) {
-          let currNode = responseDataVars[i];
-          let tagName = currNode.tagName;
-          let nodeValue = currNode.innerHTML;
-          if (tagName == "trafficmaxlimit") {
-            mainInfo.monthlyDataCapInBytes = parseInt(nodeValue);
-          } else if (tagName == "StartDay") {
-            mainInfo.startOfMonthDay = parseInt(nodeValue);
-          }
-        }
-        updatepopUpInterface();
-      }
-    };
-    httpRequest.open(
-      "GET",
-      "http://192.168.1.1/api/monitoring/start_date",
-      true
-    );
-    httpRequest.overrideMimeType("text/xml");
-    httpRequest.send();
-  }
-
-  function prepareViewData() {
-    fetchCurrentMonthData();
-    fetchOverallData();
-  }
-
-  function fillPopupContent() {
-    prepareViewData();
-    // updateInterface(); // well,that's asynchronous, so...
-  }
-  fillPopupContent();
+  browser.storage.local.get("data").then(loadDataAndShow);
 })();

6a39e223fe3950213b9b8723e26609cd9bfb8449
Maciej Howański <howanski@windowsmail.com>
2021-02-21 15:15:03 +0100

    added config to transmission management

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ Extension reads data from modem and quickly presents in readable form.
 
 As for now **only** **_Huawei B535-232_** model is supported as that's what extension was developed and tested on.
 
-1.0.2 Features:
+1.1.0 Features:
 
 - Preview how much data left till end of the month
 - Preview how much data left till end of current day
 - Preview how much monthly data should be left on oncoming days
 - Preview what data exchange speed will not exceed transfer limit till end of month
+- Transmission (Torrent client) speed steering
 
 Oncoming features:
 
-- Transmission (Torrent client) speed steering
-- More models support
+- More modem models support
 - Dynamic icon
 - Current transfer rates
--- html/options.js
+++ html/options.js
@@ -3,10 +3,14 @@
     restrictTorrentSpeed: false,
     restrictOnlyTurtleMode: false,
     showScheduleOnPopup: true,
+    transmissionAuthorizationUsername: "myUser",
+    transmissionAuthorizationPassword: "myPassword",
   };
+
   function writeLocalConfig() {
     browser.storage.local.set({ config: extensionConfig });
   }
+
   function readLocalConfig() {
     function processConfig(readConfig) {
       let configSubObject = readConfig.config;
@@ -20,12 +24,31 @@
       } else {
         scheduleSwitch.checked = false;
       }
+
+      let transmissionSwitch = document.getElementById("transmission-switch");
+      if (extensionConfig.restrictTorrentSpeed) {
+        transmissionSwitch.checked = true;
+      } else {
+        transmissionSwitch.checked = false;
+      }
+
+      let transmissionUserInput = document.getElementById("transmission-user");
+      transmissionUserInput.value =
+        extensionConfig.transmissionAuthorizationUsername;
+
+      let transmissionPasswordInput = document.getElementById(
+        "transmission-password"
+      );
+      transmissionPasswordInput.value =
+        extensionConfig.transmissionAuthorizationPassword;
+
       writeLocalConfig();
     }
     browser.storage.local.get("config").then(processConfig);
   }
 
   function registerEvents() {
+    // that's gonna need refactorization soon
     function flipScheduleSwitch(event) {
       let eventTarget = event.target;
       extensionConfig.showScheduleOnPopup = eventTarget.checked;
@@ -33,6 +56,30 @@
     }
     let scheduleSwitch = document.getElementById("schedule-switch");
     scheduleSwitch.addEventListener("change", flipScheduleSwitch);
+
+    function flipTransmissionSwitch(event) {
+      let eventTarget = event.target;
+      extensionConfig.restrictTorrentSpeed = eventTarget.checked;
+      writeLocalConfig();
+    }
+    let transmissionSwitch = document.getElementById("transmission-switch");
+    transmissionSwitch.addEventListener("change", flipTransmissionSwitch);
+
+    function changeTransmissionUser(event) {
+      let eventTarget = event.target;
+      extensionConfig.transmissionAuthorizationUsername = eventTarget.value;
+      writeLocalConfig();
+    }
+    let transmissionUserInput = document.getElementById("transmission-user");
+    transmissionUserInput.addEventListener("keyup", changeTransmissionUser);
+
+    function changeTransmissionPassword(event) {
+      let eventTarget = event.target;
+      extensionConfig.transmissionAuthorizationPassword = eventTarget.value;
+      writeLocalConfig();
+    }
+    let transmissionPasswordInput = document.getElementById("transmission-password");
+    transmissionPasswordInput.addEventListener("keyup", changeTransmissionPassword);
   }
   readLocalConfig();
   registerEvents();
